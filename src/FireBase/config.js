1a6eef8ce1565c15af137685f20482b271c1e7a8
Aiwin Thomas <aiwin.as789@gmail.com>
2023-07-12 10:03:20 +0530

    First Commit

---
+++ src/FireBase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAJzIDUV2x515tNoyUo11HkdhJQPOb993w",
+  authDomain: "olx-react-5ceaa.firebaseapp.com",
+  projectId: "olx-react-5ceaa",
+  storageBucket: "olx-react-5ceaa.appspot.com",
+  messagingSenderId: "1038441663062",
+  appId: "1:1038441663062:web:462e816af7f311e41749f7",
+  measurementId: "G-DKDGZ53C75",
+};
+export const firebase = initializeApp(firebaseConfig);
